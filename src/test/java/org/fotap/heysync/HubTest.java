ce8f3fb6b1097ff62f2eec4b5a403ac2f37593e4
peter royal <peter.royal@pobox.com>
2010-01-23 21:07:07 -0600

    move spike test into hub test

--- src/test/java/org/fotap/heysync/HubTest.java
+++ src/test/java/org/fotap/heysync/HubTest.java
@@ -1,13 +1,14 @@
 package org.fotap.heysync;
 
 import org.jetlang.core.DisposingExecutor;
+import org.jetlang.fibers.FiberStub;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.util.Collection;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 /**
  * @author <a href="mailto:peter.royal@pobox.com">peter royal</a>
@@ -15,6 +16,20 @@ import static org.mockito.Mockito.mock;
 public class HubTest {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void shouldSendAsyncMessage() {
+        Hub hub = new Hub();
+        FiberStub executor = new FiberStub();
+
+        Mouse receiver = mock(Mouse.class);
+        hub.addReceiver(receiver, executor);
+
+        hub.dispatcherFor(Mouse.class).eatCheese("cheddar");
+        verifyZeroInteractions(receiver);
+        executor.executeAllPending();
+        verify(receiver).eatCheese("cheddar");
+    }
 
     @Test
     public void shouldFailWhenTryingToGetDispatcherForNotAtAsynchronousInterface() {
